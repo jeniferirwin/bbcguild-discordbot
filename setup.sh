9b27b3f902329dd09b70f76f3762796234f72b00
Jenifer Irwin <jenifer.irwin@gmail.com>
2025-06-07 16:26:31 -0400

    change to prevent permission issues with installs

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-export $(grep -v '^#' .env | xargs) &&
-service mariadb start &&
-echo "CREATE USER IF NOT EXISTS ${BOTNAME}@localhost; GRANT ALL PRIVILEGES ON *.* TO ${BOTNAME}@localhost; FLUSH PRIVILEGES;" | mysql &&
-npm install prisma &&
+export $(grep -v '^#' .env | xargs)
+sudo service mariadb start
+sudo echo "CREATE USER IF NOT EXISTS ${BOTNAME}@localhost; GRANT ALL PRIVILEGES ON *.* TO ${BOTNAME}@localhost; FLUSH PRIVILEGES;" | mysql
+npm install prisma
 npx prisma migrate dev --name init
