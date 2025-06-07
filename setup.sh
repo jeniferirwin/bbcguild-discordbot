7bdde3d8f842d56a7eba1ab5f05ca9ea48124731
Jenifer Irwin <jenifer.irwin@gmail.com>
2025-06-07 16:20:10 -0400

    make prisma install less strict

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 export $(grep -v '^#' .env | xargs) &&
 service mariadb start &&
-echo "CREATE USER ${BOTNAME}@localhost; GRANT ALL PRIVILEGES ON *.* TO ${BOTNAME}@localhost; FLUSH PRIVILEGES;" | mysql &&
-npm install prisma -g &&
+echo "CREATE USER IF NOT EXISTS ${BOTNAME}@localhost; GRANT ALL PRIVILEGES ON *.* TO ${BOTNAME}@localhost; FLUSH PRIVILEGES;" | mysql &&
+npm install prisma &&
 npx prisma migrate dev --name init
