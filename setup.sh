5c63fa5f9e996676f9778fe1618a87df44162b46
Jenifer Irwin <jenifer.irwin@gmail.com>
2025-06-07 15:32:32 -0400

    remove unnecessary command from setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 export $(grep -v '^#' .env | xargs) &&
 service mariadb start &&
 echo "CREATE USER ${BOTNAME}@localhost; GRANT ALL PRIVILEGES ON *.* TO ${BOTNAME}@localhost; FLUSH PRIVILEGES;" | mysql &&
-cd /root &&
 npm install prisma -g &&
 npx prisma migrate dev --name init
-
