a39a6813a636a9eb596c5f958f9f3002a32f17bb
Jenifer Irwin <jenifer.irwin@gmail.com>
2025-06-07 16:34:48 -0400

    attempt to increase compatibility

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 export $(grep -v '^#' .env | xargs)
-sudo service mariadb start
-sudo echo "CREATE USER IF NOT EXISTS ${BOTNAME}@localhost; GRANT ALL PRIVILEGES ON *.* TO ${BOTNAME}@localhost; FLUSH PRIVILEGES;" | mysql
+service mariadb start
+echo "CREATE USER IF NOT EXISTS ${BOTNAME}@localhost; GRANT ALL PRIVILEGES ON *.* TO ${BOTNAME}@localhost; FLUSH PRIVILEGES;" | mysql
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+source ~/.bashrc
+nvm install 22.9.0
+nvm use 22.9.0
+npm install -g npm@11.4.1
 npm install prisma
 npx prisma migrate dev --name init
